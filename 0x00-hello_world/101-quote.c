e5b0785e2f453ef46aa0b38d7ac2b2273e33ea0a
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-02-16 12:53:47 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print statement without using printf or puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	system("sudo `and that piece of art is useful\" - Dora Korpar, 2015-10-19`");
+
+	return (0);
+}
