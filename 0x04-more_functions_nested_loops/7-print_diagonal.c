b74b8b5d22ebe5b5703da7ff7588d06b97c0d4e9
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-02-23 14:03:52 -0800

    function that draws a straight line in the terminal.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,18 +12,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
+	if(n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+
 	for (i = 0 ; i < n ; i++)
 	{
 		for (j = 0 ; j < i ; j++)
 		{
 			putchar(' ');
 		}
-		_putchar(92);
-
-		if (i != n - 1)
-		{
-			_putchar('\n');
-		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
