8f6bc48750c82647956223c41bd37b45274ea275
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-03-02 14:37:49 -0800

    function that encodes a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ *
+ * @s: string
+ *
+ * Description:
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ *
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+	int i = 0;
+	int encode[133];
+
+	encode['a'] = encode['A'] = 4;
+	encode['e'] = encode['E'] = 3;
+	encode['o'] = encode['O'] = 0;
+	encode['t'] = encode['T'] = 7;
+	encode['l'] = encode['L'] = 1;
+
+	while (s[i] != '\0')
+	{
+
+		if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'o'
+				|| s[i] == 'O' || s[i] == 't' || s[i] == 'T' || s[i] == 'l' || s[i] == 'L')
+		{
+			s[i] = encode[s[i]];
+		}
+
+		i++;
+	}
+
+	return (s);
+}
