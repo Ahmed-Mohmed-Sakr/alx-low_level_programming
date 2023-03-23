3f4fa85152ec967d4a1d25f58c0e2d4280afdafd
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-03-23 03:06:40 -0700

    program that performs simple operations.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 	op = argv[2];
 	n2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != NULL)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		return (99);
