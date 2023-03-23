cf1241e5100f6891554965f56b4fa9a56bcadd02
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-03-23 05:53:54 -0700

    function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,6 +2,21 @@
 
 
 /**
+ * print_string - print string
+ *
+ * @s: string
+ *
+ * Return: void
+ */
+void print_string(char *s)
+{
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
  * print_all -  function that prints anything.
  *
  * @format: input format
@@ -11,51 +26,26 @@
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0;
+	unsigned int i = 0,  n = strlen(format);
 	va_list list;
-	int sp = 0;
-	char *s;
-	unsigned int n = strlen(format);
 
 	va_start(list, format);
-
 	while (i < n)
 	{
 		if (format[i] == 'c')
-		{
-			printf("%c",va_arg(list, int));
-			sp = 1;
-		}
+			printf("%c", va_arg(list, int));
 		else if (format[i] == 'i')
-		{
-			printf("%d",va_arg(list, int));
-			sp = 1;
-		}
+			printf("%d", va_arg(list, int));
 		else if (format[i] == 'f')
-		{
-			printf("%f",va_arg(list, double));
-			sp = 1;
-		}
+			printf("%f", va_arg(list, double));
 		else if (format[i] == 's')
-		{
-			s = va_arg(list, char*);
+			print_string(va_arg(list, char*));
+		else
+			continue;
 
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s",s);
-
-			sp = 1;
-		}
-
-
-
-		if (sp && i != n - 1)
+		if (i != n - 1)
 			printf(", ");
-
-		sp = 0;
 		i++;
 	}
-
 	va_end(list);
 }
