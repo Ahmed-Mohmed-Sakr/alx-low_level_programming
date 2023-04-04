ee19fa6eae9721329af9373b93857b39d2e2037b
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-04-04 10:21:27 -0700

    check first that there is a refrance to list then deRefrance it :):):)

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *list = *head;
+	listint_t *list;
 	listint_t *cur;
 
 	if (head == NULL)
 		return;
+
+	*list = *head;
 
 	while (list != NULL)
 	{
