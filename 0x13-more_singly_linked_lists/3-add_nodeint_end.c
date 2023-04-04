749e683949857f0c64ae2139ff8048e79b25027a
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-04-04 08:56:24 -0700

    fixing variabe name

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newNode = malloc(sizeof(listint_t));
-	listint_t *list = (*haed);
+	listint_t *list = (*head);
 
 
 	if (newNode == NULL)
