9522f9ed154b7227274e6b2a2cd1feffe0487500
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-04-10 12:02:20 -0700

    trying to fixing bug > file that come out don't have 664 permisions ??

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
 	if (f_from == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
 
-	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	f_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (f_to == -1)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 
